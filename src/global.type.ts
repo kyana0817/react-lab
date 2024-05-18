790b5aa4c5696feaa3e4628ad104dff8df2bc25a
kazeyanagi <kyana0817@gmail.com>
2024-05-19 00:22:38 +0900

    :white_check_mark: fix fiber type error

--- src/global.type.ts
+++ src/global.type.ts
@@ -1,11 +1,12 @@
-import { extend, Object3DNode } from '@react-three/fiber'
+
+import { extend, ThreeElement  } from '@react-three/fiber'
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js'
 
 extend({ TextGeometry })
 
 declare module '@react-three/fiber' {
   interface ThreeElements {
-    textGeometry: Object3DNode<TextGeometry, typeof TextGeometry>
+    textGeometry: ThreeElement<typeof TextGeometry>
   }
 }
 
