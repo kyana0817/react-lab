34593583f46c38a2ea410388ba6b03d8c2fa99ef
kazeyanagi <kyana0817@gmail.com>
2025-03-13 22:36:27 +0900

    fix type of useWorker

--- src/hooks/useWorker.ts
+++ src/hooks/useWorker.ts
@@ -1,7 +1,8 @@
 import crypto from 'crypto'
 
 type FP<T extends (...args: never[]) => unknown> =  T extends (...args: infer I) => unknown? I: never
-type ActionRecord = Record<string, (...args: never[])=>unknown>
+type ActionRecord = Record<string, (...args: never[])=>UnwrapPromise<unknown>>
+type UnwrapPromise<T> = T extends Promise<infer U>? U: T
 
 type ClientMessage<
   T extends ActionRecord,
@@ -18,12 +19,17 @@ const randomString = (len=16) => {
     .toString('hex')
 }
 
+const isPromise = (value: unknown | Promise<unknown>): value is Promise<unknown> => {
+  return value != null && typeof value === 'object' && 'then' in value && typeof value.then === 'function'
+}
+
 export const expose = <
   T extends ActionRecord
 >(actions: T, worker: DedicatedWorkerGlobalScope) => {
   worker.onmessage = async ({ data }: MessageEvent) => {
     const { id, type, args }: ClientMessage<T> = JSON.parse(data)
-    const payload = await actions[type](...args)
+    const result = actions[type](...args)
+    const payload = isPromise(result)? await result: result
     worker.postMessage(JSON.stringify({ id, payload }))
   }
   return actions
@@ -36,11 +42,11 @@ export const useWorker = <T extends ActionRecord = ActionRecord>(worker: Worker)
       id, type, args
     }))
 
-    return new Promise<ReturnType<T[K]>>((resolve) => {
+    return new Promise<UnwrapPromise<ReturnType<T[K]>>>((resolve) => {
       worker.addEventListener('message', function callback({ data }: MessageEvent){
         const { id: reciveId, payload } = JSON.parse(data) as {
           id: string;
-          payload: ReturnType<T[K]>
+          payload: UnwrapPromise<ReturnType<T[K]>>
         }
         if (id !== reciveId) return
 
